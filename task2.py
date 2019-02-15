6051fabccb3add20ebc247a9ec5e77f4853d67c6
charanpreet13 <c.oberoi@dal.ca>
2019-02-15 11:17:34 -0800

    task2 completed

---
+++ task2.py
@@ -0,0 +1,17 @@
+log=open('access.log','r')
+logget=open('get.log','a')
+logput=open('put.log','a')
+logpost=open('post.log','a')
+logdelete=open('delete.log','a')
+for line in log:
+        line=line.strip()
+        if 'get '.upper() in line.upper():
+                logget.write(line)
+        elif 'post '.upper() in line.upper():
+                logpost.write(line)
+        elif 'PUT '.upper() in line.upper():
+                logput.write(line)
+        elif 'delete '.upper() in line.upper():
+                logdelete.write(line)
+
+
